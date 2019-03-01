12c6dc6d235655a4d43d419587e4b73345fff1e0
Jerry Chen <zgchen559@gmail.com>
2019-03-01 15:49:57 -0800

    app nav

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
+const App = () => {
+  return (
+    <Fragment className="App">
+      <nav className="navigation">
+        Edit <code>src/App.js</code> and save to reload.
+        <a>
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>React</li>
+          </ul>
+        </a>
+      </nav>
+      <main>
+        <div className="leftContent">
+          <img src={logo} className="App-logo" alt="logo" />
+          <form action="">
+            <h2 className="formTitle">Add a bookmark </h2>
+            <div>
+              <label htmlFor="linkTitle" />
+            </div>
+            <div>x</div>
+            <button>x</button>
+          </form>
+        </div>
+        <div className="rightContent">
+          <h2>15m</h2>
+        </div>
+      </main>
+    </Fragment>
+  );
+};
 
 export default App;
